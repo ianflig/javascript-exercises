389cda44d614fc0d8ea5fd99e5168fab5e97460e
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-18 18:30:37 -0300

    feat: solution exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) { /* ...args recolectar todos los valores que vengan después y crea un array que posteriormente se filtra con includes */
+    const newArray = [];
+    arr.forEach(
+        (item) => {
+            if(!args.includes(item)){
+                newArray.push(item);
+            }
+        }
+    );
+    return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
