b76a98f3c6688532f9b59cf7754d89c4165dc96d
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-18 17:45:44 -0300

    feat: solution exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str
+    .split("")
+    .reverse()
+    .join("")
 };
+
+console.log(reverseString('hello there')) // returns 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
