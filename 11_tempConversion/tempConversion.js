86ac2e99421ff957c42151a7858d82b206e72125
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-18 23:43:44 -0300

    feat: solution exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+function convertToOneDecimal(num){
+  return Math.round(num * 10) / 10
+}
+
+const convertToCelsius = function(far) {
+    return convertToOneDecimal((far - 32) * 5/9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+    return convertToOneDecimal((cel * 9/5 + 32));
 };
+
+console.log(convertToCelsius(32), 
+convertToFahrenheit(0))
 
 // Do not edit below this line
 module.exports = {
