f8668777ef055a0f9e9df26b3d740b36a22be373
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-24 20:52:13 -0300

    solve exercise 13

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,6 +1,11 @@
-const factorial = function() {
-
+const factorial = function(number) {
+  let factorial = 1;
+  if (Number.isInteger(number) && number >= 0) {
+	for (let i = number; i >= 1; i--){
+    factorial *= i;
+    }
+    } else {return};
+  return factorial;
 };
-
 // Do not edit below this line
 module.exports = factorial;
