f357d99a4f26a18b547958397976fc596fe54234
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-24 20:44:34 -0300

    solve exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, number) => (acc + number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, number) => acc * number);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+	for (let i = number; i >= 1; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
