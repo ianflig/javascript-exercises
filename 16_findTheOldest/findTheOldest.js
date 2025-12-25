51dc910bd82d254e328d2308fc01e36b6fd6cab5
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-25 01:25:07 -0300

    solve exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.map(person => ({
+        ...person,
+        // si no tiene fecha de muerte se le pone la actual
+        // tambien se puede usar (new Date()).getFullYear()
+        // para obtener el año actual
+        yearsOld : ((person.yearOfDeath || 2025) - person.yearOfBirth) 
+    }))
+    .sort((a, b) => ((b.yearsOld) - (a.yearsOld)))
+    .reduce((a, b) => (b > a) ? b : a); 
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
