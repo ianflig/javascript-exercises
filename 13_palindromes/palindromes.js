29624a82723fae3f7360acaa816731fb793e74f7
ianflig <129326345+ianflig@users.noreply.github.com>
2025-12-24 21:10:52 -0300

    solver exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // valid characters
+    const alphanumerical = "abcdefghijklmnñopqrstuvwxyz0123456789";
 
+    // get clean string
+    let cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter(character => alphanumerical.includes(character))
+    .join("");
+
+    //reversed string
+    let reversedString = cleanedString.split("").reverse().join("");
+    console.log(reversedString)
+    return reversedString == cleanedString ? true : false;
 };
-
+console.log(palindromes("racecar"));
 // Do not edit below this line
 module.exports = palindromes;
