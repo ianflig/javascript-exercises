d05bc1fc294a0dda6788f18a0c9c4f1d1992461d
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-27 20:08:13 -0300

    first test passes

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,22 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  if (typeof obj !== "object") {
+    if (obj === value) {
+      return true;
+    }
+  } else {
+    let object = Object.values(obj);
+
+    for (let i = 0; i < object.length; i++) {
+      if (contains(object[i], value)) {
+        return true;
+      } else {
+        contains(object[i], value);
+      }
+    }
+
+    return false;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
