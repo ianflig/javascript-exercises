c325967a9bf36f065fe88e8155471a931991a95d
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-27 16:39:55 -0300

    solve exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function (number) {
+  // no need to do "if number is 1 return number", it's covered with 0 validation
+  if (number === 0) {
+    return 1;
+  }
+  if (number < 0 || !Number.isInteger(number)) {
+    return undefined;
+  }
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
