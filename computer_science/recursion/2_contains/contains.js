3baf56684b05b64b8e0766c9153ff160b0a8bb16
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-28 11:39:41 -0300

    solve exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -6,14 +6,8 @@ const contains = function (obj, value) {
   if (typeof obj !== "object" || obj === null) {
     return false;
   }
-  // console.log(obj);
+
   for (let object of Object.values(obj)) {
-    // console.log("--------------");
-    // console.log("INSIDE LOOP");
-    // console.log("--------------");
-    // console.log(Object.values(obj));
-    // console.log("sending -> ", object);
-    // console.log("------------------------------------------");
     if (contains(object, value)) {
       return true;
     }
@@ -24,31 +18,3 @@ const contains = function (obj, value) {
 
 // Do not edit below this line
 module.exports = contains;
-
-console.log(
-  contains(
-    {
-      test: { test: 44 },
-      array: {
-        arrayinside: [42],
-        anotherButWithString: ["45"],
-        anotherObj: { empty: null, car: NaN, yes: "yes" },
-      },
-    },
-    NaN,
-  ),
-);
-
-// console.log(
-//   typeof NaN,
-//   NaN === NaN,
-//   Number.isNaN({
-//     test: { test: 44 },
-//     array: {
-//       arrayinside: [42],
-//       anotherButWithString: ["45"],
-//       anotherObj: { empty: null, car: NaN, yes: "yes" },
-//     },
-//   }),
-//   Number.isNaN("d"),
-// );
