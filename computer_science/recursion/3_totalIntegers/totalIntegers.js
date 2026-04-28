a7c89b56c0e3cce37fc8ace7f42d64d6e22b0b23
ianflig <129326345+ianflig@users.noreply.github.com>
2026-04-28 14:24:01 -0300

    1st test

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,21 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  if (Number.isInteger(obj)) return true;
+
+  if (typeof obj !== "object") return;
+
+  let total = 0;
+  for (let number of Object.values(obj)) {
+    if (totalIntegers(number)) {
+      total += totalIntegers(number);
+    }
+  }
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
+
+// console.log(totalIntegers([5, 2, [3, 2]]));
+
+// console.log(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]])); // returns 7
+// console.log(totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } })); // returns 4
