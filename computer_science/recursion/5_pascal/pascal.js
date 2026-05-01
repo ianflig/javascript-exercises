82cd48780134c8c82ba634e3f1778bb02a5b53a9
ianflig <129326345+ianflig@users.noreply.github.com>
2026-05-01 18:36:17 -0300

    1st & 2nd test

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,7 +1,10 @@
 const pascal = function (number) {
   if (number === 1) return [1];
 
-  //   const result;
+  let result = [0].concat(pascal(number - 1));
+  for (let i = 0; i < result.length - 1; i++) {
+    result[i] = result[i] + result[i + 1];
+  }
 
   return result;
 };
